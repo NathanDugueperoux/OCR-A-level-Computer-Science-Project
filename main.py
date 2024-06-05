ba8b107a67fd91747d5293fca8809f928a5eef35
N_Dugueperoux18 <nathandugueperoux554@gmail.com>
2024-06-05 12:54:31 +0100

    df

--- main.py
+++ main.py
@@ -1,27 +1,24 @@
-import tkinter as tk
-import telemetrix_uno_r4
-
-window = tk.Tk()
-window.geometry("1000x500")
-text = tk.Label(window, text="Controls")
-frame = tk.Frame(window, width=1000, height=1000, bg="red", highlightbackground="black", highlightthickness=1 )
-frame.pack(pady=100, padx=100)
+import pygame
+import sys
 
 
-def track_motion(event):
-    x = event.x
-    y = event.y
-    print(f"({x}, {y})")
+class Game:
+    def __init__(self):
+        pygame.init()
+        self.screen = pygame.display.set_mode((1280, 720))
+        self.clock = pygame.time.Clock()
+
+    def run(self):
+        while True:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    sys.exit()
+                if event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_UP
+                    print("hello")
+            self.clock.tick(60)
 
 
-def move_robot(event):
-    if event.char == "e":
-        print("go up")
-    if event.char == "q":
-        print("go down")
-
-
-frame.bind("<Motion>", track_motion)
-window.bind("<Key>", move_robot)
-
-window.mainloop()
+game = Game()
+game.run()
