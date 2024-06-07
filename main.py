8db4a4fea5391ddfb07d433cdc0a9d4a742edb2f
N_Dugueperoux18 <nathandugueperoux554@gmail.com>
2024-06-07 12:54:51 +0100

    s

--- main.py
+++ main.py
@@ -1,24 +1,43 @@
 import pygame
 import sys
+#
+# PATH = "assets/images/"
+#
+#
+# class Game:
+#     def __init__(self):
+#         pygame.init()
+#         self.window = pygame.display.set_mode((640, 480))
+#         self.clock = pygame.time.Clock()
+#         self.cloud = pygame.image.load(f"{PATH}cloud_1.png")
+#
+#     def run(self):
+#         while True:
+#             self.window.blit(self.cloud, (200, 200))
+#
+#             for event in pygame.event.get():
+#                 if event.type == pygame.QUIT:
+#                     pygame.quit()
+#                     sys.exit()
+#                 if event.type == pygame.KEYDOWN:
+#                     if event.key == pygame.K_UP:
+#                         print("hello")
+#             self.clock.tick(60)
+#
+#
+# game = Game()
+# game.run()
 
+pygame.init()
+window = pygame.display.set_mode((640, 480))
+clock = pygame.time.Clock()
+cloud = pygame.image.load("assets/images/cloud_1.png")
 
-class Game:
-    def __init__(self):
-        pygame.init()
-        self.screen = pygame.display.set_mode((1280, 720))
-        self.clock = pygame.time.Clock()
+while True:
+    window.blit(cloud, (100, 200))
 
-    def run(self):
-        while True:
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    pygame.quit()
-                    sys.exit()
-                if event.type == pygame.KEYDOWN:
-                    if event.key == pygame.K_UP
-                    print("hello")
-            self.clock.tick(60)
-
-
-game = Game()
-game.run()
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+    clock.tick(60)
