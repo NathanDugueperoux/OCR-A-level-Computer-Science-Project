8f20e634fbbdff1ee5737f2cc9097c36c13798c8
N_Dugueperoux18 <nathandugueperoux554@gmail.com>
2024-07-05 15:05:20 +0100

    refactored find_adjacent_enemy_territories() so it works

--- algorithms.py
+++ algorithms.py
@@ -10,27 +10,31 @@ Austria = Territories("Blue", "Austria", 0, ["Germany"])
 
 existing_territories = [England, Denmark, Brazil, France, Austria, United_States, Germany]
 
+red_team = [i for i in existing_territories if i.get_info()[0] == "Red"]
+blue_team = [i for i in existing_territories if i.get_info()[0] == "Blue"]
+
+# creates a dictionary with the key being home territory and the value being a list of territories the home territory can attack.
+
 def find_adjacent_enemy_territories(territory: object):
-    adjacent_enemy_territories = []
+    adjacent_enemy_territories = {}
+    temp = []
     for i in territory.get_info()[3]:
         for j in existing_territories:
             if i == j.get_info()[1]:
                 if j.get_info()[0] != territory.get_info()[0]:
-                    adjacent_enemy_territories.append(i)
+                    temp.append(i)
+        adjacent_enemy_territories[territory.get_info()[1]] = temp
     return adjacent_enemy_territories
 
-def attack_decision_making(enemy_territories: list):
-    pass
+# iterates through the red team list and runs each item through find_adjacent_enemy_territories() putting resulting dictionary in the list attackable_enemy_territories.
 
+attackable_enemy_territories = []
 
-red_team = [i for i in existing_territories if i.get_info()[0] == "Red"]
-blue_team = [i for i in existing_territories if i.get_info()[0] == "Blue"]
+for i in blue_team: 
+    if find_adjacent_enemy_territories(i)[i.get_info()[1]] == []:
+        pass
+    else:
+        attackable_enemy_territories.append(find_adjacent_enemy_territories(i))
 
-attackable_enemy_territories = set([])
-
-for i in red_team:
-    print(i)
-    temp = find_adjacent_enemy_territories(i)
-    for j in temp:
-        attackable_enemy_territories.add(j)
+print(attackable_enemy_territories)
         
