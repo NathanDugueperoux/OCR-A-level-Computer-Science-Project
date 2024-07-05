04d96d8815d8f938eaa0af5209386e8446201f7b
Nathan Dugueperoux <nathanlucasdugueperoux@gmail.com>
2024-07-05 23:25:04 +0100

    added fortifying_decision_making()

--- algorithms.py
+++ algorithms.py
@@ -4,8 +4,8 @@ England = Territories("Red", "England", 5, ["Denmark", "United_States", "France"
 Denmark = Territories("Red", "Denmark", 0, ["England", "United_States", "Brazil"])
 Brazil = Territories("Red", "Brazil", 0, ["Denmark"])
 United_States = Territories("Red", "United_States", 0, ["Denmark", "England", "France", "Germany"])
-France = Territories("Blue", "France", 10, ["England", "United_States", "Germany"])
-Germany = Territories("Blue", "Germany", 0, ["France", "United_States", "Austria"])
+France = Territories("Blue", "France", 15, ["England", "United_States", "Germany"])
+Germany = Territories("Blue", "Germany", 1, ["France", "United_States", "Austria"])
 Austria = Territories("Blue", "Austria", 0, ["Germany"])
 
 existing_territories = [England, Denmark, Brazil, France, Austria, United_States, Germany]
@@ -28,11 +28,21 @@ def find_adjacent_enemy_territories(territory: object):
 
 def fortifying_decision_making(enemy_territories: list):
     priority = None
+    difference = 0
+    total = 0
     for i in range(len(attackable_enemy_territories)):
         for key, value in attackable_enemy_territories[i].items():
             for j in existing_territories:
                 if key == j.get_info()[1]:
-                    pass
+                    for k in value:
+                        for l in existing_territories:
+                            if k == l.get_info()[1]:
+                                total += l.get_info()[2]
+                    temp = difference
+                    difference = total - j.get_info()[2]
+                    if difference > temp:
+                        priority = j
+    return priority
                     
 
 # iterates through the red team list and runs each item through find_adjacent_enemy_territories() putting resulting dictionary in the list attackable_enemy_territories.
@@ -47,3 +57,6 @@ for i in red_team:
 
 # test
 
+print(attackable_enemy_territories)
+
+print(fortifying_decision_making(attackable_enemy_territories).get_info()[1])
